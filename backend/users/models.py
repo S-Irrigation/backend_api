a36fda5e0906c4b2d7cc41ec805b37d8bb10b26b
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-01-22 08:41:52 +0100

    adding users and certains fonctionnalities

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
+import email
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+from __future__ import annotations
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+
+#services=service.models.Service
+
+class User(AbstractUser,models.Model):
+    username=models.CharField(max_length=10 , unique=True)
+    telephones = ArrayField(models.CharField(max_length=200), blank=True)
+    USERNAME_FIELD = 'username'
+    objects=UserManager()
+    def __str__(self):
+        return self.username
---
+++ backend/users/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import generics, permissions
+from rest_framework import serializers
+from .models import User
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+# User Serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email' ,'telephones','last_name' , 'first_name' , 'telephones')
+
+# Register Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password' , 'last_name' , 'first_name' ,'telephones')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(self.validated_data ['username'],  
+        self.validated_data['email'], self.validated_data['password']  , last_name=validated_data['last_name'], first_name=validated_data['first_name'] ,telephones=validated_data['telephones'])
+
+        return user
+
+# Change Password
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)

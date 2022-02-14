44609f65cd5a44cde1fa57e54e8cb40ab14fd78b
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-02-14 14:10:50 +0100

    working on url farm

--- backend/vanneGestion/urls.py
+++ backend/vanneGestion/urls.py
@@ -6,9 +6,9 @@ from vanneGestion.views import CreateChamps, ListChamps, RUDChamps
 
 
 urlpatterns = [
-    path('creerChamp',CreateChamps.asview()),
-    path('champ/<int:pk>' ,RUDChamps.asview()),
-    path('listChamp/' ,ListChamps.asview({
+    path('creerChamp',CreateChamps.as_view()),
+    path('champ/<int:pk>' ,RUDChamps.as_view()),
+    path('listChamp/' ,ListChamps.as_view({
         'get':'get'
     }))
    

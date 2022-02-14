5be047b1c72f426c1d6a4970eac574af5a993c5c
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-02-14 13:03:25 +0100

    working on farm

--- backend/vanneGestion/urls.py
+++ backend/vanneGestion/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path
 
+from backend.vanneGestion.views import CreateChamps
+
 
 
 urlpatterns = [
+    path('creerChamp',CreateChamps.asview()),
    
 ]
--- backend/vanneGestion/views.py
+++ backend/vanneGestion/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from backend.vanneGestion.models import Champ, Vanne
+from backend.vanneGestion.serializers import ChampsSerializer, VanneSerializer
+from rest_framework import generics, serializers ,viewsets,permissions
+from rest_framework.response import Response
 
 # Create your views here.
+class CreateChamps(generics.CreateAPIView):
+    queryset=Champ.objects.all()
+    serializer_class=ChampsSerializer
+
+class RUDChamps(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Champ.objects.all()
+    serializer_class=ChampsSerializer
+class ListChamps(viewsets.ViewSet):
+    def get(self ,request):
+        queryset=Champ.objects.all()
+        serializer=ChampsSerializer(queryset,many=True)
+        return Response(serializer.data)
+class CreateVanne(generics.CreateAPIView):
+    queryset=Vanne.objects.all()
+    serializer_class=VanneSerializer

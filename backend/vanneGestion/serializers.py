16163323d6330b9cb0733dd7dae01978a034d3a6
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-03-05 21:10:48 +0100

    Adding file champs

--- backend/vanneGestion/serializers.py
+++ backend/vanneGestion/serializers.py
@@ -7,7 +7,7 @@ from vanneGestion.models import Capteur, Champ, MicroControleur, Noeud, Vanne
 class ChampsSerializer(serializers.ModelSerializer):
    class Meta:
        model=Champ
-       fields=('nomChamp','address','proprietaire','description','creer_le' ,'modifier_le')
+       fields=('id' ,'nomChamp','address','proprietaire','description','creer_le' ,'modifier_le')
 class NoeudSerializer(serializers.ModelSerializer):
     class Meta:
         model=Noeud
@@ -15,7 +15,7 @@ class NoeudSerializer(serializers.ModelSerializer):
 class VanneSerializer(serializers.ModelSerializer):
     class Meta:
         model=Vanne
-        fields=('status' ,'nomNoeud' , 'champ')
+        fields=('id','status' ,'nomNoeud' , 'champ')
 
 class CapteurSerializer(serializers.ModelSerializer):
     class Meta:

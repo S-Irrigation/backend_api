dae0a3d1ced82e417761a0c56f3d52f8fa6f1357
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-03-10 17:49:32 +0100

    removing description from vanne

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,6 @@
 import email
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+#from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
@@ -8,7 +8,6 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser,models.Model):
     username=models.CharField(max_length=10 , unique=True)
-    telephones = ArrayField(models.CharField(max_length=5),default=list)
     USERNAME_FIELD = 'username'
     objects=UserManager()
     def __str__(self):
